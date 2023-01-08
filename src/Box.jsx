502b74f78b714185a0cc3f3010403aeda38fcc90
minhoseong <minhos3389@gmail.com>
2023-01-09 02:15:18 +0900

    Add useFrame from @react-three/fiber

---
+++ src/Box.jsx
@@ -0,0 +1,19 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+
+export default function Box(props) {
+  const ref = useRef()
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 1 * delta
+    ref.current.rotation.y += 0.5 * delta
+    // ref.current.position.y = Math.sin(state.clock.getElapsedTime() * 2) / 2
+  })
+
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe />
+    </mesh>
+  )
+}
